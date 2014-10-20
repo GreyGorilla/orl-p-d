9693a1f1e96f04098dfc4bf8c2e9a677571760e2
Robb Schiller <robbschiller@gmail.com>
2014-10-20 12:07:35 -0400

    fixes, experimenting

--- build.js
+++ build.js
@@ -1,12 +1,11 @@
 'use strict'
 
 var  Metalsmith = require('metalsmith'),
-  markdown = require('metalsmith-markdown'),
   sass = require('metalsmith-sass'),
   watch = require('metalsmith-watch'),
   templates = require('metalsmith-templates'),
-  ignore = require('metalsmith-ignore'),
   permalinks = require('metalsmith-permalinks'),
+  ignore = require('metalsmith-ignore'),
   metadata = require('metalsmith-metadata'),
   serve = require('serve-static'),
   connect = require('connect');
@@ -18,7 +17,9 @@ process.argv.forEach(function(val, index, array) {
 })
 
 var metalsmith = Metalsmith(__dirname)
-  .use(sass())
+  .use(sass({
+    outputStyle: 'expanded'
+  }))
   .use(metadata({
     designers: 'data/designers.json',
     resources: 'data/resources.json'
@@ -30,14 +31,15 @@ var metalsmith = Metalsmith(__dirname)
     'lib/**/.bower.json',
     'sass/**/*'
   ]))
-  .use(markdown())
   .use(templates({
-    engine: 'handlebars',
+    engine: 'hogan',
     directory: './src/templates',
     partials: {
       head: 'partials/head',
       header: 'partials/header', 
-      footer: 'partials/footer'
+      footer: 'partials/footer', 
+      designers: 'partials/designers',
+      resources: 'partials/resources'
     }
   }))
   .use(permalinks({
